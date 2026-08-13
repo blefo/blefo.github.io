7ada78842a7d2674cb44cc029ffc60e528cb6816
blefo <71429321+blefo@users.noreply.github.com>
2026-08-13 17:14:18 +0200

    Two-panel layout, humanized ASCII face, live star/download metrics

--- app.js
+++ app.js
@@ -1,57 +1,68 @@
 (function () {
   "use strict";
 
-  // Pupils sit in a 3x3 socket interior, so each eye can look in 9 directions.
+  // A human face drawn with ASCII. Each pupil moves in a 3x3 socket interior,
+  // so both eyes can look up/down and left/right to follow the cursor.
   function renderFace(pupilLX, pupilLY, pupilRX, pupilRY) {
-    var W = 34;
-    var H = 14;
+    var W = 40;
+    var H = 15;
     var g = [];
-    for (var r = 0; r < H; r++) g.push(new Array(W).fill(" "));
+    var r, i;
+    for (r = 0; r < H; r++) g.push(new Array(W).fill(" "));
 
-    var left = 4;
-    var right = 29;
-    var top = 0;
-    var bottom = 12;
-
-    for (var x = left + 1; x < right; x++) g[top][x] = "_";
-    for (x = left + 1; x < right; x++) g[bottom][x] = "_";
-    g[top + 1][left] = "/";
-    g[top + 1][right] = "\\";
-    g[bottom - 1][left] = "\\";
-    g[bottom - 1][right] = "/";
-    for (var y = top + 2; y < bottom - 1; y++) {
-      g[y][left] = "|";
-      g[y][right] = "|";
+    function put(y, x, s) {
+      for (var j = 0; j < s.length; j++) g[y][x + j] = s[j];
     }
 
+    // Hair (dome).
+    put(0, 15, "~~~~~~~~");
+    put(1, 12, "~~~~~~~~~~~~~~");
+    put(2, 9, "~~~~~~~~~~~~~~~~~~~~");
+
+    // Head outline.
+    g[3][8] = "/";
+    g[3][31] = "\\";
+    g[4][7] = "/";
+    g[4][32] = "\\";
+    for (r = 5; r <= 11; r++) {
+      g[r][6] = "|";
+      g[r][33] = "|";
+    }
+    g[12][7] = "\\";
+    g[12][32] = "/";
+    g[13][8] = "\\";
+    g[13][31] = "/";
+    put(14, 10, "____________");
+
+    // Eyebrows.
+    put(5, 13, "___");
+    put(5, 25, "___");
+
+    // Eyes: 5-wide almond sockets with a 3x3 pupil interior.
     var eyes = [
-      { x: 9, y: 4 },
-      { x: 19, y: 4 },
+      { x: 12, y: 6 },
+      { x: 24, y: 6 },
     ];
     var pupils = [
       { x: pupilLX, y: pupilLY },
       { x: pupilRX, y: pupilRY },
     ];
 
-    for (var i = 0; i < eyes.length; i++) {
+    for (i = 0; i < eyes.length; i++) {
       var e = eyes[i];
-      for (var ex = e.x; ex < e.x + 5; ex++) g[e.y][ex] = "_";
-      for (ex = e.x; ex < e.x + 5; ex++) g[e.y + 4][ex] = "_";
-      for (var row = 1; row <= 4; row++) {
-        g[e.y + row][e.x] = "|";
-        g[e.y + row][e.x + 4] = "|";
-      }
+      put(e.y, e.x, "(   )");
+      put(e.y + 1, e.x, "(   )");
+      put(e.y + 2, e.x, "(   )");
       var p = pupils[i];
-      g[e.y + 1 + p.y][e.x + 1 + p.x] = "o";
+      g[e.y + p.y][e.x + 1 + p.x] = "o";
     }
 
-    var mx = 15;
-    var my = 10;
-    g[my][mx] = "\\";
-    g[my][mx + 1] = "_";
-    g[my][mx + 2] = "_";
-    g[my][mx + 3] = "_";
-    g[my][mx + 4] = "/";
+    // Nose.
+    put(9, 20, "|");
+    put(10, 20, "~");
+
+    // Mouth.
+    put(11, 16, "\\______/");
 
     return g
       .map(function (row) {
